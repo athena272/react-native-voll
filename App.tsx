c554bb317d97b5f85d7a9d9b03b4483fe22a63df
athena272 <guilhermera272@gmail.com>
2024-09-06 20:25:12 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { THEMAS } from './src/@types'
 export default function App() {
   return (
     <NativeBaseProvider theme={THEMAS}>
+      <StatusBar backgroundColor={THEMAS.colors.blue[800]}/>
       <Home />
     </NativeBaseProvider>
   );
