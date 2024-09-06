417edae8bc17723ffebfc93b3df42f2540c46d94
athena272 <guilhermera272@gmail.com>
2024-09-06 19:17:45 -0300

    Create index.tsx

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Tela principal</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
