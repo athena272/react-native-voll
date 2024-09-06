10a0a5d071b03e65110d75442b8014a0126b6eaf
athena272 <guilhermera272@gmail.com>
2024-09-06 19:17:58 -0300

    Create Themas.ts

---
+++ src/@types/Themas.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "native-base";
+
+export const THEMAS = extendTheme({
+    colors: {
+        gray: {
+            300: '#8D8D99'
+        },
+        blue: {
+            500: '#339CFF',
+            800: '#0B3B60',
+        },
+        white: '#fff',
+        black: '#000'
+    }
+})
