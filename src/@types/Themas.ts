31918a5aaad2c8cfd39fe7505e91ccd4c24519b9
athena272 <guilhermera272@gmail.com>
2024-09-06 20:17:36 -0300

    Update Themas.ts

--- src/@types/Themas.ts
+++ src/@types/Themas.ts
@@ -11,5 +11,12 @@ export const THEMAS = extendTheme({
         },
         white: '#fff',
         black: '#000'
+    },
+    fontSizes: {
+        xs: 12,
+        sm: 14,
+        md: 16,
+        lg: 20,
+        xl: 24
     }
 })
