dd1d881c7070b024b92b678c62c8d3604da3676b
athena272 <guilhermera272@gmail.com>
2024-09-06 21:25:00 -0300

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,13 @@
-import { Image, VStack } from "native-base"
+import { Image, VStack, Text } from "native-base"
 import Logo from '../../assets/Logo.png'
 
 export default function Login() {
     return (
         <VStack flex={1} alignItems="center" p={5}>
             <Image source={Logo} alt="Logo Voll"/>
+            <Text>
+                Faça login em sua conta
+            </Text>
         </VStack>
     )
 }
