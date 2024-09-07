4abdb228ff7a84903c1a9fc301f992c6f71f6ea6
athena272 <guilhermera272@gmail.com>
2024-09-07 10:57:36 -0300

    Update indext.tsx

--- src/pages/Register/indext.tsx
+++ src/pages/Register/indext.tsx
@@ -1,4 +1,4 @@
-import { Image, VStack, Text, Box, Link } from "native-base"
+import { Image, Box, Checkbox, ScrollView, Text } from "native-base"
 import { useState } from "react"
 import Logo from '../../assets/Logo.png'
 import { sections } from './sections'
@@ -8,9 +8,11 @@ import InpuText from "../../components/InputText"
 
 export default function Register() {
     const [numSection, setNumSection] = useState(0)
+    const firstSection = 0
+    const lastSection = (sections.length - 1)
 
     function nextSection() {
-        if (numSection < (sections.length - 1)) {
+        if (numSection < lastSection) {
             setNumSection(prev => prev + 1)
         }
     }
@@ -22,8 +24,8 @@ export default function Register() {
     }
 
     return (
-        <VStack flex={1} alignItems="center" justifyContent={"center"} p={5}>
-            <Image source={Logo} alt="Logo Voll" />
+        <ScrollView flex={1} p={5}>
+            <Image source={Logo} alt="Logo Voll" alignSelf={"center"} />
             <Title>
                 {sections[numSection].title}
             </Title>
@@ -36,13 +38,35 @@ export default function Register() {
                     />
                 ))}
             </Box>
-            <BTN onPress={prevSection} bgColor={'gray.400'}>
-                Voltar
-            </BTN>
-            <BTN onPress={nextSection} mt={4}>
-                Avançar
-            </BTN>
-        </VStack>
+            <Box>
+                {
+                    numSection === lastSection &&
+                    <Text color="blue.800" fontWeight="bold" fontSize="md" mt="2" mb={2}>
+                        Selecione os planos:
+                    </Text>
+                }
+                {sections[numSection].checkbox.map(input => (
+                    <Checkbox
+                        key={input.id}
+                        value={input.value}
+                    >
+                        {input.value}
+                    </Checkbox>
+                ))}
+            </Box>
+            {
+                numSection > firstSection &&
+                <BTN onPress={prevSection} bgColor={'gray.400'}>
+                    Voltar
+                </BTN>
+            }
+            {
+                numSection < lastSection &&
+                <BTN onPress={nextSection} mt={numSection === firstSection ? 10 : 4} mb={20} >
+                    Avançar
+                </BTN>
+            }
+        </ScrollView>
     )
 }
 
