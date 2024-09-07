af5d415a0c95696e2dd0f4327e3053cd6bb5d445
athena272 <guilhermera272@gmail.com>
2024-09-07 20:13:31 -0300

    Update index.tsx

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -35,6 +35,7 @@ export default function Register() {
                         key={input.id}
                         label={input.label}
                         placeholder={input.placeholder}
+                        secureTextEntry={input.secureTextEntry}
                     />
                 ))}
             </Box>
