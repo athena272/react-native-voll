17aee7b5d2064687afe288c0c907ffa19ede484f
athena272 <guilhermera272@gmail.com>
2024-09-07 20:09:59 -0300

    Update index.tsx

--- src/components/AppointmentCard/index.tsx
+++ src/components/AppointmentCard/index.tsx
@@ -1,19 +1,30 @@
 import { Text, Avatar, VStack } from 'native-base'
 import BTN from '../Button'
 
-export default function AppointmentCard() {
+type AppointmentCard = {
+    name: string;
+    photo: string;
+    specialty: string;
+    date?: string;
+    hasBeAttended?: boolean;
+    hasScheduled?: boolean;
+}
+
+export default function AppointmentCard({ name, photo, specialty, date, hasBeAttended, hasScheduled }: AppointmentCard) {
     return (
-        <VStack w="100%" bg="white" p="5" borderRadius="lg" shadow="2">
+        <VStack w="100%" bg={hasBeAttended ? 'blue.200' : 'white'} p="5" borderRadius="lg" shadow="2" mb={5}>
             <VStack flexDir="row">
-                <Avatar size="lg" source={{ uri: "https://github.com/andreocunha.png" }} />
-                <VStack>
-                    <Text>Dr. André Cunha</Text>
-                    <Text>Cardiologista</Text>
-                    <Text>20/04/2023</Text>
+                <Avatar size="lg" source={{ uri: photo }} />
+                <VStack pl={4}>
+                    <Text>{name}</Text>
+                    <Text>{specialty}</Text>
+                    <Text>{date}</Text>
                 </VStack>
             </VStack>
             <BTN mt={4}>
-                Agendar consulta
+                {
+                    hasScheduled ? 'Cancelar' : 'Agendar consulta'
+                }
             </BTN>
         </VStack>
     )
