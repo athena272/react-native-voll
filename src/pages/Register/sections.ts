80b39814bc599f5c20e7ce834213e4ee0c672f64
athena272 <guilhermera272@gmail.com>
2024-09-07 10:57:38 -0300

    Update sections.ts

--- src/pages/Register/sections.ts
+++ src/pages/Register/sections.ts
@@ -14,6 +14,18 @@ export const sections = [
                 label: 'E-mail',
                 placeholder: 'Digite seu e-mail'
             },
+            {
+                id: 3,
+                label: 'Crie uma senha',
+                placeholder: 'Insira sua senha',
+                secureTextEntry: true,
+            },
+            {
+                id: 4,
+                label: 'Confirme sua senha',
+                placeholder: 'Insira sua senha',
+                secureTextEntry: true,
+            }
         ],
         checkbox: []
     },
@@ -26,7 +38,27 @@ export const sections = [
                 id: 1,
                 label: 'CEP',
                 placeholder: 'Digite seu CEP'
-            }
+            },
+            {
+                id: 2,
+                label: 'Endereço',
+                placeholder: 'Insira seu endereço'
+            },
+            {
+                id: 3,
+                label: 'Número',
+                placeholder: 'Insira seu número'
+            },
+            {
+                id: 4,
+                label: 'Complemento',
+                placeholder: 'Insira seu complemento'
+            },
+            {
+                id: 5,
+                label: 'Telefone',
+                placeholder: '(00) 00000-0000'
+            },
         ],
         checkbox: []
     },
@@ -43,6 +75,30 @@ export const sections = [
             {
                 id: 2,
                 value: 'Unimed'
+            },
+            {
+                id: 3,
+                value: 'Bradesco'
+            },
+            {
+                id: 4,
+                value: 'Amil'
+            },
+            {
+                id: 5,
+                value: 'Biosaúde'
+            },
+            {
+                id: 6,
+                value: 'Biovida'
+            },
+            {
+                id: 7,
+                value: 'Outros'
+            },
+            {
+                id: 8,
+                value: 'Não tenho plano'
             }
         ]
     }
