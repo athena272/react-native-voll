156e7b8d3660bdd57f12534a3fa953468c6b5c79
athena272 <guilhermera272@gmail.com>
2024-09-07 20:09:56 -0300

    Update index.tsx

--- src/pages/Tabs/Consultation/index.tsx
+++ src/pages/Tabs/Consultation/index.tsx
@@ -1,12 +1,46 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Divider, ScrollView } from 'native-base'
 import AppointmentCard from '../../../components/AppointmentCard'
+import Title from '../../../components/Title'
+import BTN from '../../../components/Button'
 
 export default function Consultation() {
     return (
-        <VStack p={5}>
-            <Text>Consulta</Text>
-            <AppointmentCard />
-        </VStack>
+        <ScrollView p={5}>
+            <Title color="blue.500">Minhas consultas</Title>
+            <BTN mt={5} mb={5}>Agendar nova consulta</BTN>
 
+            <Title color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Próximas consultas</Title>
+            <AppointmentCard
+                name='Dr. Andre'
+                specialty='Cardiologista'
+                photo='https://github.com/andreocunha.png'
+                date='20/04/2023'
+                hasScheduled
+            />
+            
+            <Divider mt={5} />
+            <Title color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Consultas passadas</Title>
+            <AppointmentCard
+                name='Dr. Andre'
+                specialty='Cardiologista'
+                photo='https://github.com/andreocunha.png'
+                date='20/04/2023'
+                hasBeAttended
+            />
+            <AppointmentCard
+                name='Dr. Andre'
+                specialty='Cardiologista'
+                photo='https://github.com/andreocunha.png'
+                date='20/04/2023'
+                hasBeAttended
+            />
+            <AppointmentCard
+                name='Dr. Andre'
+                specialty='Cardiologista'
+                photo='https://github.com/andreocunha.png'
+                date='20/04/2023'
+                hasBeAttended
+            />
+        </ScrollView >
     )
 }
