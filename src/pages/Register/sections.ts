2ff10027b3abf4e0e8cf0f4f5947e0e2a24a5d9d
athena272 <guilhermera272@gmail.com>
2024-09-07 09:34:34 -0300

    Create sections.ts

---
+++ src/pages/Register/sections.ts
@@ -0,0 +1,31 @@
+export const sections = [
+    // First section
+    {
+        id: 1,
+        title: 'Insira alguns dados básicos',
+        inputText: [
+            {
+                id: 1,
+                label: 'Nome',
+                placeholder: 'Digite seu nome completo'
+            },
+            {
+                id: 2,
+                label: 'E-mail',
+                placeholder: 'Digite seu e-mail'
+            },
+        ]
+    },
+    // Second section
+    {
+        id: 2,
+        title: 'Agora, mais alguns dados sobre você:',
+        inputText: [
+            {
+                id: 1,
+                label: 'CEP',
+                placeholder: 'Digite seu CEP'
+            }
+        ]
+    }
+]
