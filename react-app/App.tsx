2a0b4208a764e65ff52bd579e398e31cab853097
athena272 <guilhermera272@gmail.com>
2024-09-08 00:12:35 -0300

    Update App.tsx

--- react-app/App.tsx
+++ react-app/App.tsx
@@ -1,7 +1,19 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEMAS } from './src/styles'
 import Routes from './src/Routes';
+import { useEffect } from 'react';
+import { api } from './src/services/api';
+
 export default function App() {
+  useEffect(() => {
+    async function getData() {
+      const result = await api.get('/paciente')
+      console.log("🚀 ~ getData ~ result:", result.data)
+    }
+
+    getData()
+  }, [])
+
   return (
     <NativeBaseProvider theme={THEMAS}>
       <StatusBar backgroundColor={THEMAS.colors.blue[800]} />
